87e321c7d8852003c33995fe32e223b8bb18844a
MartinBarker <martinbarker99@gmail.com>
2020-05-22 13:48:39 -0700

    ex

--- martinbarker.me/server.js
+++ martinbarker.me/server.js
@@ -1,18 +1,27 @@
+//setup express app
 const express = require('express');
 const app = express();
 const port = 3000;
+
 //Loads the handlebars module
 const handlebars = require('express-handlebars');
+
 //Sets our app to use the handlebars engine
 app.set('view engine', 'handlebars');
+
 //Sets handlebars configurations (we will go through them later on)
 app.engine('handlebars', handlebars({
-layoutsDir: __dirname + '/views/layouts',
+    layoutsDir: __dirname + '/views/layouts',
 }));
+
+//Tells app to use '/public' folder for static files
 app.use(express.static('public'))
+
+//homr page route
 app.get('/', (req, res) => {
-//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-res.render('main', {layout : 'index'});
+    //Serves the body of the page ("main.handlebars") to the container ("index.handlebars")
+    res.render('main', {layout : 'index'});
 });
 
+//render and startup server
 app.listen(port, () => console.log(`App listening to port ${port}`));
