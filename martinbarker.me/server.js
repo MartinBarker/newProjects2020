4463e91a2cc1cb58ab155cacf02c4a0cdbc8b462
martinbarker <martinbarker99@gmail.com>
2020-09-04 20:19:09 -0700

    port change

--- martinbarker.me/server.js
+++ martinbarker.me/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 //port and hosting info
-const port = 3000;
+const port = 8080;
 
 //connect to mongodb server
 var mongoUtil = require( './static/assets/js/mongoUtils' );
